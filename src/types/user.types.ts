97198486c63a2eed685314c38044232a27127436
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 08:29:13 +0200

    Created User Types

---
+++ src/types/user.types.ts
@@ -0,0 +1,12 @@
+type UserTypes = {
+   id?: number;
+   user_id?: number;
+   first_name: string;
+   last_name: string;
+   email: string;
+   password: string;
+   created_at?: Date;
+   updated_at?: Date;
+};
+
+export default UserTypes;
