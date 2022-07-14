d126873c13d95a47ce25f6810d3eb35cbb0753f0
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 08:47:20 +0200

    Added User Controller signup

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,32 @@
-class UserController {}
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+import User from "../models/User";
+import config from "../config";
+
+class UserController {
+   signup = async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+   ): Promise<Response> => {
+      try {
+         let user = await User.signup(req.body);
+         let jwtSecret: string = config.jwtSecret || "";
+         let token = jwt.sign({ id: user.id }, jwtSecret);
+         let { password, ...userData } = user;
+
+         return res.status(201).json({
+            status: "success",
+            data: userData,
+            token,
+         });
+      } catch (err) {
+         return res.status(500).json({
+            message: `unable to create: ${(err as Error).message}`,
+         });
+      }
+   };
+}
 
 export default UserController;
