976ae9f4dd6ab650479c246a81b6a10ffa6ac7bf
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 10:20:55 +0200

    Added login route

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -6,5 +6,6 @@ const userController = new UserController();
 const userRoutes: IRouter = Router();
 
 userRoutes.post("/signup", userController.signup);
+userRoutes.post("/login", userController.login);
 
 export default userRoutes;
