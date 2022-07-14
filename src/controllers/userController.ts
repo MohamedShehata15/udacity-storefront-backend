a7cee66c6dbb72497c18f47c708180fbed81d142
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 10:20:20 +0200

    Added user controller login function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 import User from "../models/User";
 import config from "../config";
+import { AppError } from "./../utils/appError";
 
 class UserController {
    signup = async (
@@ -24,6 +26,34 @@ class UserController {
       } catch (err) {
          return res.status(500).json({
             message: `unable to create: ${(err as Error).message}`,
+         });
+      }
+   };
+
+   login = async (req: Request, res: Response, next: NextFunction) => {
+      try {
+         let { email, password } = req.body;
+         let user = await User.login(email, password);
+
+         // Check user exits
+         if (!user) return next(new AppError("Invalid credentials", 401));
+
+         // check password is correct
+         if (!(await bcrypt.compare(password, user.password)))
+            return next(new AppError("Invalid credentials", 401));
+
+         let { password: _, ...userData } = user;
+
+         const token = jwt.sign({ id: user.id }, config.jwtSecret || "");
+
+         return res.status(200).json({
+            status: "success",
+            data: userData,
+            token,
+         });
+      } catch (err) {
+         return res.status(500).json({
+            message: `unable to login: ${(err as Error).message}`,
          });
       }
    };
