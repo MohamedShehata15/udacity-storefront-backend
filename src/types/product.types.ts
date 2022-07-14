f30205a40b25ea5bbcc6ef79ab4ccb5268088292
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 12:01:41 +0200

    Created Product Types

---
+++ src/types/product.types.ts
@@ -0,0 +1,11 @@
+type ProductTypes = {
+   id?: number;
+   name: string;
+   description?: string;
+   price: number;
+   user_id: string;
+   created_at?: Date;
+   updated_at?: Date;
+};
+
+export default ProductTypes;
