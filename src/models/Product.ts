00c5b95333cdceecc95a6e0e4de7a0a4f200172e
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 12:05:54 +0200

    Added Product Model

---
+++ src/models/Product.ts
@@ -0,0 +1,10 @@
+import db from "../database";
+import ProductTypes from "./../types/product.types";
+
+class Product {
+   // Create Product
+   // Get Product
+   // Get All Products
+}
+
+export default Product;
