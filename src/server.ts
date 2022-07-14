91a2994a4c4ec2685b27a6731fbaf77d573f7fb8
mohamed159716 <mohamed159716@yahoo.com>
2022-07-14 10:20:40 +0200

    Handled Errors and exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
-import express, { Request, Response, Application } from "express";
+import express, { Request, Response, NextFunction, Application } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 
 import routes from "./routes/index";
+import { AppError } from "./utils/appError";
+
+/**
+ * Handle UnCaught Exception
+ */
+process.on("uncaughtException", (err) => {
+   console.log(err.name, err.message);
+   console.log("Uncaught Exception... Shutting down");
+   process.exit(1);
+});
 
 const app: Application = express();
 const address: string = "http://localhost:4000";
@@ -12,6 +22,32 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.use("/", routes);
+
+/**
+ * Route Not Found Handler
+ */
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+/**
+ * Global Error Handler
+ */
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+   res.status(err.statusCode || 500).json({
+      status: err.status,
+      message: err.message,
+   });
+});
+
+/**
+ * Unhandled Rejections
+ */
+process.on("unhandledRejection", (err: Error) => {
+   console.log(err.name, err.message);
+   console.log("Unhandled Rejection... Shutting down");
+   process.exit(1);
+});
 
 app.listen(PORT, function () {
    console.log(`starting app on: ${address}`);
